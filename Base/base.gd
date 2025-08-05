017b712d2ef6e9ffbe246775e58d52bdbad51498
Sergio Andrés Castellanos Landazábal <serancaslan@gmail.com>
2025-08-05 18:29:26 -0400

    path and base health

---
+++ Base/base.gd
@@ -0,0 +1,18 @@
+extends Node3D
+@onready var label_3d: Label3D = $Label3D
+@export var max_health : int = 5
+
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		label_3d.text = str(current_health) +"/"+str(max_health)
+		var white = Color.WHITE
+		var red = Color.RED
+		label_3d.modulate = red.lerp(white,float(current_health)/float(max_health))
+		if current_health < 1:
+			get_tree().reload_current_scene()
+		
+func _ready() -> void:
+	current_health = max_health
+func take_damage() -> void:
+	current_health -= 1
